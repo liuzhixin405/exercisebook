cff0853cec41373cbc5287993cb87c74338079b5
liuzhixin405 <164910441@qq.com>
2022-09-15 23:00:32 +0800

    u
    u

---
+++ 程序集注入/RuntimeStore/Program.cs
@@ -0,0 +1,3 @@
+using System;
+
+public class Program { public static void Main() {  }}
---
+++ 程序集注入/WebApiForStartupDiagnostics/Program.cs
@@ -0,0 +1,40 @@
+namespace WebApplication1
+{
+    /// <summary>
+    /// ³ÌÐò¼¯×¢Èë
+    /// https://localhost:7266/services  
+    /// https://localhost:7266/diag
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
