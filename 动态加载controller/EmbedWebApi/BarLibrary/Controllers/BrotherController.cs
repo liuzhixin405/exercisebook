8bbc9c62a1d1c785429e0dde216740c324577b13
liuzhixin405 <164910441@qq.com>
2022-10-01 10:43:52 +0800

    u
    u

--- 动态加载controller/EmbedWebApi/BarLibrary/Controllers/BrotherController.cs
+++ 动态加载controller/EmbedWebApi/BarLibrary/Controllers/BrotherController.cs
@@ -12,16 +12,6 @@ namespace BarLibrary.Controllers
 
         public BrotherController()
         {
-            //if (serviceDescriptors == null)
-            //    throw new ArgumentNullException($"{nameof(serviceDescriptors)} is null");
-            //var provider = serviceDescriptors.GetType().GetProperty("RootProvider", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-            //var serviceField = provider.GetType().GetField("_realizedServices", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-            //var serviceval = serviceField.GetValue(provider);
-            //var funcType = serviceField.FieldType.GetGenericArguments()[1].GetGenericArguments()[0];
-            //var method = serviceField.FieldType.GetMethods().Where(s => s.Name == "GetOrAdd").ToArray()[2];
-            //var result = typeof(BrotherController).GetMethods().FirstOrDefault(s => s.Name == "GetFunc").MakeGenericMethod(funcType);
-            //method.Invoke(serviceval, new object[] { typeof(InjectService), result });//那不好servicecollection
-
         }
         [HttpGet("AddDll")]
         public string AddDll()
@@ -42,7 +32,7 @@ namespace BarLibrary.Controllers
         public string Chear([FromServices] IServiceProvider serviceProvider)
         {
            using var scope = serviceProvider.CreateAsyncScope();
-            var injectService= scope.ServiceProvider.GetRequiredService<IInjectService>();
+            var injectService= scope.ServiceProvider.GetRequiredService<IInjectService>();//单例new对象，动态注入也是new,所以这个点不好处理
             return injectService.Get();
         }
 
