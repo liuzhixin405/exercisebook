89b58fe591ccd3088a9314e3288d2c917c75cfef
liuzhixin405 <164910441@qq.com>
2022-11-27 14:06:59 +0800

    u
    u

---
+++ 敏捷软件开发demo/Design/BubbleTemplate/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace BubbleTemplate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    /// <summary>
+    /// 改造前
+    /// </summary>
+    public class BubbleSorter
+    {
+        static int operations = 0;
+        public static int Sort(int[] array)
+        {
+            operations = 0;
+            if (array.Length <= 1)
+                return operations;
+            for (int nextToLast = array.Length - 2; nextToLast <= 0; nextToLast--)
+                for (int index = 0; index <= nextToLast; index++)
+                    CompareAndSwap(array, index);
+            return operations;
+        }
+
+        private static void CompareAndSwap(int[] array, int index)
+        {
+            if (array[index] > array[index + 1])
+               Swap(array,index);
+            operations++;
+        }
+
+        private static void Swap(int[] array, int index)
+        {
+            int temp = array[index];
+            array[index] = array[index + 1];
+            array[index + 1] = temp;
+        }
+    }
+    /// <summary>
+    /// 改造后
+    /// </summary>
+
+    public abstract class BubbleSorterBase
+    {
+        private int operations = 0;
+        protected int length = 0;
+        protected int DoSort()
+        {
+            operations = 0;
+            if (length <= 1)
+                return operations;
+            for (int nextToLast = length - 2; nextToLast <= 0; nextToLast--)
+                for (int index = 0; index <= nextToLast; index++)
+                {
+                    if (OutOfOrder(index))
+                        Swap(index);
+                    operations++;
+                }
+
+            return operations;
+        }
+
+        protected abstract void Swap(int index);
+        protected abstract bool OutOfOrder(int index);
+    }
+
+    public class DoubleBubbleSorter : BubbleSorterBase
+    {
+        private double[] array = null;
+        public int Sort(double[] inArray)
+        {
+            array = inArray;
+            length = array.Length;
+            return DoSort();
+        }
+        protected override bool OutOfOrder(int index)
+        {
+            return array[index] > array[index + 1];
+        }
+
+        protected override void Swap(int index)
+        {
+            double temp = array[index];
+            array[index] = array[index + 1];
+            array[index + 1] = temp;
+        }
+    }
+}
---
+++ 敏捷软件开发demo/Design/TemplateDesign/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace TemplateDesign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region one
+            //bool done = false;
+            //while (!done)
+            //{
+            //    string fahrString = Console.In.ReadLine();
+            //    if (fahrString == null || fahrString.Length == 0)
+            //        done = true;
+            //    else
+            //    {
+            //        double fahr = double.Parse(fahrString);
+            //        double celcius = 5.0 / 9.0 * (fahr - 32);
+            //        Console.Out.WriteLine($"F={fahr},C={celcius}");
+            //    }
+            //}
+            //Console.Out.WriteLine("ftoc exit"); 
+            #endregion
+
+            #region two
+            new FtoCTemplateMethod().Run();
+            #endregion
+        }
+    }
+
+    public abstract class Application
+    {
+        private bool isDone = false;
+        protected abstract void Init();
+        protected abstract void Idle();
+        protected abstract void Cleanup();
+        protected void SetDone()=>isDone = true;
+        
+        protected bool Done()=>isDone;
+        public void Run()
+        {
+            Init();
+            while (!Done())
+                Idle();
+            Cleanup();
+        }
+
+    }
+
+    public class FtoCTemplateMethod : Application
+    {
+        private TextReader input;
+        private TextWriter output;
+        protected override void Cleanup()
+        {
+            output.WriteLine("ftoc exit");
+        }
+
+        protected override void Idle()
+        {
+
+            string fahrString = input.ReadLine();
+            if (fahrString == null || fahrString.Length == 0)
+                SetDone();
+            else
+            {
+                double fahr = double.Parse(fahrString);
+                double celcius = 5.0 / 9.0 * (fahr - 32);
+                output.WriteLine($"F={fahr},C={celcius}");
+            }
+
+        }
+
+        protected override void Init()
+        {
+            input = Console.In;
+            output = Console.Out;
+        }
+    }
+}
