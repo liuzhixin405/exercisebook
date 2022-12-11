e5a47bc00395f95b589ff505684bae1afa423ca3
liuzhixin405 <164910441@qq.com>
2022-12-11 15:28:42 +0800

    u
    u

---
+++ BlazorSignalR/BlazorServerSignalRApp/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using BlazorServerSignalRApp.Data;
+using Microsoft.AspNetCore.ResponseCompression;
+using BlazorServerSignalRApp.Server.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddResponseCompression(opts=>{
+   opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new []{"application/octet-stream"});
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapHub<ChatHub>("/chathub");
+app.MapFallbackToPage("/_Host");
+
+app.Run();
