7e077e06f46602ba42a4d84c8b00d4be6162e2ac
liuzhixin405 <164910441@qq.com>
2022-12-17 14:18:38 +0800

    u
    u

---
+++ 三层基操/ThreeLayerArchitecture/Architecture.Infrastructure/Hashing/DefaultPasswordHasher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architecture.Infrastructure.Hashing
+{
+    public class DefaultPasswordHasher:IHashingService
+    {
+    }
+}
