e5bcedab02e22ed618cde8ac1cd6048d30a9cc24
liuzhixin405 <164910441@qq.com>
2023-02-11 13:00:00 +0800

    u
    u

---
+++ 方法模板抽象/状态模式一/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace 状态模式一
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TrafficLight trafficLight = new TrafficLight(new GreenLightState());
+            trafficLight.Show();
+            trafficLight.SetState(new YellowLightState());
+            trafficLight.Show();
+            trafficLight.SetState(new RedLightState());
+            trafficLight.Show();
+        }
+    }
+
+    /// <summary>
+    /// 定义状态的抽象类
+    /// </summary>
+    public abstract class TrafficLighState
+    {
+        public abstract void Display();
+    }
+
+    #region 具体实现
+    public class RedLightState : TrafficLighState
+    {
+        public override void Display()
+        {
+            Console.WriteLine("RedLight");
+        }
+    }
+    public class GreenLightState : TrafficLighState
+    {
+        public override void Display()
+        {
+            Console.WriteLine("GreenLight");
+        }
+    }
+    public class YellowLightState : TrafficLighState
+    {
+        public override void Display()
+        {
+            Console.WriteLine("YellowLight");
+        }
+    } 
+    #endregion
+
+    /// <summary>
+    /// 状态控制类
+    /// </summary>
+    public class TrafficLight
+    {
+        private TrafficLighState trafficLighState;
+        public TrafficLight(TrafficLighState trafficLighState)
+        {
+            this.trafficLighState = trafficLighState;
+        }
+        public void SetState(TrafficLighState state)
+        {
+            trafficLighState = state;
+        }
+        public void Show()
+        {
+            trafficLighState.Display();
+        }
+    }
+}
---
+++ 方法模板抽象/状态模式二/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Numerics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace 状态模式二
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lamborghini = new Car();
+
+            Display(lamborghini.CurrentState);
+            lamborghini.TaskAction(Action.Start);
+
+            Display(lamborghini.CurrentState);
+            lamborghini.TaskAction(Action.Start);
+
+            Display(lamborghini.CurrentState);
+            lamborghini.TaskAction(Action.Accelerate); //加速
+
+            Display(lamborghini.CurrentState);
+            lamborghini.TaskAction(Action.Stop);
+
+            Display(lamborghini.CurrentState);
+            /* result
+                车辆当前状态: Stopped
+                车辆当前状态:Started
+                车辆当前状态:Started
+                车辆当前状态:Running
+                车辆当前状态:Stopped
+            */
+        }
+        static void Display(State state)
+        {
+            Console.WriteLine($"车辆当前状态:{state}");
+        }
+    }
+    /// <summary>
+    /// 模拟汽车的状态和驾驶员的动作判断车辆当前的状态
+    /// </summary>
+    public class Car
+    {
+        private State state = State.Stopped;
+        public State CurrentState { get { return state; } }
+
+        public void TaskAction(Action action)
+        {
+            state = (state, action) switch
+            {
+                (State.Stopped, Action.Start) => State.Started,
+                (State.Started, Action.Accelerate) => State.Running,
+                (State.Started, Action.Stop) => State.Stopped,
+                (State.Running, Action.Stop) => State.Stopped,
+                _ => state
+            };
+        }
+    }
+    public enum State { Stopped, Started, Running }
+    public enum Action { Stop, Start, Accelerate }
+}
