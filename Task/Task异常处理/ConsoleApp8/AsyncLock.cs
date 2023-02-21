1c78b67c74debada4c317e76fb2d3cd8265e2bd8
liuzhixin405 <164910441@qq.com>
2023-02-21 23:08:22 +0800

    u
    u

---
+++ Task/Task异常处理/ConsoleApp8/AsyncLock.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class AsyncLock
+    {
+        private readonly SemaphoreSlim semaphore;
+
+        public AsyncLock()
+        {
+            semaphore = new SemaphoreSlim(1);
+        }
+        public ValueTask<IDisposable> LockAsync()
+        {
+            Task wait = semaphore.WaitAsync();
+            if(wait.IsCompletedSuccessfully)
+            {
+                return new(new LockReleaser(this));
+            }
+            else
+            {
+                return LockAsyncInternal(this, wait);
+
+                static async ValueTask<IDisposable> LockAsyncInternal(AsyncLock self,Task waitTask)
+                {
+                    await waitTask.ConfigureAwait(false);
+                    return new LockReleaser(self);
+                }
+            }
+        }
+
+        private class LockReleaser : IDisposable
+        {
+            private AsyncLock target;
+            internal LockReleaser(AsyncLock target)
+            {
+                this.target = target;
+            }
+            public void Dispose()
+            {
+                if (target == null)
+                    return;
+                AsyncLock tmp = target;
+                target = null;
+                try
+                {
+                    tmp.semaphore.Release();
+                }
+                catch (Exception){ }
+            }
+        }
+    }
+}
---
+++ Task/Task异常处理/ConsoleApp8/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        private static AsyncLock  alock = new AsyncLock();
+        static async Task Main(string[] args)
+        {
+            using(await alock.LockAsync())
+            {
+               //TODO:await 代码
+            }
+        }
+       
+    }
+}
