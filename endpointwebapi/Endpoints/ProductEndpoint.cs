498594656cbb456e1427109b0cd5f724c76fac0b
liuzhixin405 <164910441@qq.com>
2023-04-13 10:56:56 +0800

    u
    u

--- endpointwebapi/Endpoints/ProductEndpoint.cs
+++ endpointwebapi/Endpoints/ProductEndpoint.cs
@@ -9,28 +9,32 @@ using System.Reflection.Metadata.Ecma335;
 
 namespace webapi.Endpoints
 {
-    public class ProductEndpoint : IEndpoint<List<Product>, ProductRequestDto, ProductDbContext>
+    public class ProductEndpoint : IEndpoint<List<Product>, PageInput<ProductRequestDto>, ProductDbContext>
     {
         public void AddRoute(IEndpointRouteBuilder app)
         {
-            app.MapPost("api/prodct", async ([FromBody]ProductRequestDto request, ProductDbContext dbContext) =>
+            app.MapPost("api/prodct", async ([FromBody] PageInput<ProductRequestDto> request, ProductDbContext dbContext) =>
             {
                return await  HandleAsync(request, dbContext);
             });
         }
-        private static readonly Func<ProductDbContext, string,string,decimal?,DateTime?, IAsyncEnumerable<Product>> GetProductAsync =
-            EF.CompileAsyncQuery((ProductDbContext ctx,string name,string description, decimal? price,DateTime? createtime) =>
-            ctx.Products.Where(x => string.IsNullOrEmpty(name) || x.Name.Contains(name))
+        private static readonly Func<ProductDbContext,int,int,bool,string, string,string,decimal?,DateTime?, IAsyncEnumerable<Product>> GetProductAsync =
+            EF.CompileAsyncQuery((ProductDbContext ctx,int pageIndex,int pageSize,bool sortType,string sortField,string name,string description, decimal? price,DateTime? createtime) =>
+            ctx.Products
+            .Where(x => string.IsNullOrEmpty(name) || x.Name.Contains(name))
             .Where(x => string.IsNullOrEmpty(description) || x.Description.Contains(description))
             .Where(x => !price.HasValue || price == x.Price)
             .Where(x => !createtime.HasValue || createtime == x.CreateDateTime)
+            //.OrderBy(sortField,sortType) //表达式不能翻译TODD:
+            .OrderByDescending(x=>x.Id)
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
             );
         
-            
-        public async Task<List<Product>> HandleAsync(ProductRequestDto request,ProductDbContext productDbContext)
+        public async Task<List<Product>> HandleAsync(PageInput<ProductRequestDto> request,ProductDbContext productDbContext)
         {
             List<Product> list = new List<Product>(); 
-            await foreach (var item in GetProductAsync(productDbContext, request.Name,request.Description,request.Price,request.CreateDateTime))
+            await foreach (var item in GetProductAsync(productDbContext,request.PageIndex,request.PageRows,request.Desc, request.SortField,request.Search.Name,request.Search.Description,request.Search.Price,request.Search.CreateDateTime))
             {
                 list.Add(item);
             } ;
---
+++ endpointwebapi/OrderByExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace webapi
+{
+    public static class OrderByExtensions
+    {
+        public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, string propertyName, bool isDescending)
+        {
+            var type = typeof(T);
+            var parameter = Expression.Parameter(type, "p");
+            var property = type.GetProperty(propertyName);
+            var propertyAccess = Expression.MakeMemberAccess(parameter, property);
+            var orderByExp = Expression.Lambda(propertyAccess, parameter);
+            var methodName = isDescending ? "OrderByDescending" : "OrderBy";
+            var resultExp = Expression.Call(typeof(Queryable), methodName, new Type[] { type, property.PropertyType }, source.Expression, Expression.Quote(orderByExp));
+            return source.Provider.CreateQuery<T>(resultExp);
+        }
+    }
+}
