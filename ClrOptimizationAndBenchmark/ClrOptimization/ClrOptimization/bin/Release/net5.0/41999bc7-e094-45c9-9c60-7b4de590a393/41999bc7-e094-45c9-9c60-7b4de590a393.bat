c12df0b53b2bd6cd566cd37a7d60a666bcc97dd3
liuzhixin405 <164910441@qq.com>
2023-08-08 18:30:20 +0800

    delete other

--- ClrOptimizationAndBenchmark/ClrOptimization/ClrOptimization/bin/Release/net5.0/41999bc7-e094-45c9-9c60-7b4de590a393/41999bc7-e094-45c9-9c60-7b4de590a393.bat
+++
@@ -1,3 +0,0 @@
-call dotnet restore  /p:UseSharedCompilation=false /p:BuildInParallel=false /m:1 /p:Deterministic=true /p:Optimize=true -r win-x64
-call dotnet build -c Release  /p:UseSharedCompilation=false /p:BuildInParallel=false /m:1 /p:Deterministic=true /p:Optimize=true -r win-x64
-call dotnet publish -c Release  /p:UseSharedCompilation=false /p:BuildInParallel=false /m:1 /p:Deterministic=true /p:Optimize=true -r win-x64
