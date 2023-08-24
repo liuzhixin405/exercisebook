0df731da8e6a97332facffad38e6aa417446ad9d
164910441@qq.com <164910441@qq.com>
2023-08-24 18:44:44 +0800

    update

--- DataFlowDemo/BufferBlockDemo/Program.cs
+++ DataFlowDemo/BufferBlockDemo/Program.cs
@@ -22,19 +22,44 @@ namespace BufferBlockDemo
 
             // Create an ActionBlock<int> object that prints values
             // to the console.
-            var actionBlock = new ActionBlock<int>(n => Console.WriteLine(n));
+            //var actionBlock = new ActionBlock<int>(n => Console.WriteLine(n));
 
-            // Post several messages to the block.
-            for (int i = 0; i < 3; i++)
+            //// Post several messages to the block.
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    actionBlock.Post(i * 10);
+            //}
+
+            //// Set the block to the completed state and wait for all
+            //// tasks to finish.
+            //actionBlock.Complete();
+            //actionBlock.Completion.Wait();
+
+
+
+
+            BufferBlock<Student> bufferBlock = new BufferBlock<Student>(new DataflowBlockOptions {  BoundedCapacity= 2});
+            ActionBlock<Student> actionBlock = new ActionBlock<Student> ( data=>Run(data), new ExecutionDataflowBlockOptions { BoundedCapacity = 10, MaxDegreeOfParallelism = DataflowBlockOptions.Unbounded } );
+            bufferBlock.LinkTo( actionBlock );
+            int i = 10;
+            while (i != 0)
             {
-                actionBlock.Post(i * 10);
+                i--;
+               await bufferBlock.SendAsync(new Student { Id = i ,Name = $"test{i}"});
             }
-
-            // Set the block to the completed state and wait for all
-            // tasks to finish.
-            actionBlock.Complete();
-            actionBlock.Completion.Wait();
             Console.ReadKey();
         }
+
+        static void Run(Student student)
+        {
+            Console.WriteLine($"Student :{student.Id} ，Name :{student.Name}");
+        }
+    } //使用Channel最佳
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Age { get; set; }
     }
 }
