a4faf772a4030a8f354cc7b45a914fcf180bb8b7
164910441@qq.com <164910441@qq.com>
2024-03-24 17:36:18 +0800

    update

---
+++ 方法模板抽象/代理模式/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace 代理模式
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IRequest<String> request = new RealRequest();
+            request.Request("proxy");
+            
+            ITarget adpter = new Adaptee<AdapteeImpl>();
+            adpter.Request();
+        }
+    }
+
+    internal interface IRequest<T>
+    {
+        void Request(T request);
+    }
+   
+
+    internal class RealRequest : IRequest<string>
+    {
+        public void Request(string request)
+        {
+            Console.WriteLine($"Request: {request}");
+        }
+    }
+
+    internal class ProxyRequest : IRequest<string>
+    {
+        private readonly IRequest<string> _request;
+        public ProxyRequest(IRequest<string> request)
+        {
+            _request = request;
+        }
+        public void Request(string request)
+        {
+            _request.Request(request);
+        }
+    }
+
+    /// <summary>
+    /// 适配器
+    /// </summary>
+    internal interface ITarget
+    {
+        void Request();
+    }
+    public class TargetImpl : ITarget
+    {
+        public void Request()
+        {
+            Console.WriteLine("this is a real request");
+        }
+    }
+    internal interface IAdaptee
+    {
+        void SpecifiedRequest();
+    }
+    public class AdapteeImpl : IAdaptee
+    {
+        public void SpecifiedRequest()
+        {
+            Console.WriteLine("this is a adapter for request");
+        }
+    }
+    internal abstract class GenericAdapterBase<T>:ITarget where T: IAdaptee,new()
+    {
+        public virtual void Request()
+        {
+            new T().SpecifiedRequest();
+        }
+    }
+
+    internal class Adaptee<T> : GenericAdapterBase<T> where T: IAdaptee,new()
+    {
+        public override void Request()
+        {
+            base.Request();
+        }
+    }
+}
