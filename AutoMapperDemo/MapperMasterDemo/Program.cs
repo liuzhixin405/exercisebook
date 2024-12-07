cc5b1e09df85546d20b8f551b43606d0dcb4a064
liuzhixin405 <164910441@qq.com>
2024-12-07 21:10:47 +0800

    up
    up

---
+++ AutoMapperDemo/MapperMasterDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using Mapster;
+
+namespace MapsterDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var followModel = new FollowModel()
+            {
+                Name = "John",
+                Email = "john@gmail.com",
+                Phone = "1234567890"
+            };
+
+            var followerDTO = followModel.Adapt<FollowerDTO>();
+            Console.WriteLine(followerDTO.Name);
+            Console.WriteLine(followerDTO.Email);
+            Console.WriteLine(followerDTO.Phone);
+        }
+    }
+
+    public class FollowModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+    public class FollowerDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
