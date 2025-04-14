464c88c433e35c9a6c35017cd3f90dcbd6806966
刘志新 <164910441@qq.com>
2025-04-14 10:20:37 +0800

    更新

--- net-spot/webapi/Services/IBqAccountService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace webapi.Services
-{
-    public interface IBqAccountService
-    {
-        Task<BqAccount> GetByUserAndCurrency(long uid, long cid);
-        Task  InsertAccount(int uid, string email, int currencyId);
-        Task<bool> UpdateAmount(int uid, int currencyId, decimal amount, decimal frozenAmount);
-    }
-}
